729ffda615af479f0acf5a0e6afbcbfbb642f439
Utsav <utsavsomaiya4464@gmail.com>
2025-04-19 10:16:01 +0530

    Remove unused code

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,31 +2,24 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
-
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
+     * @use HasFactory<UserFactory>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    use HasFactory;
+
+    use Notifiable;
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var string[]
      */
     protected $hidden = [
         'password',
